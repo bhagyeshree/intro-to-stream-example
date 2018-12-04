e73c7229c4e22c37976dcb3bfa2541d8c3bb622a
PaulDimalante <dimalantep@gmail.com>
2018-12-04 12:40:45 -0500

    added the method to check One and return one value for Assignment 2

---
+++ src/test/java/AssignmentTwoTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.List;
+import org.hamcrest.MatcherAssert;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+
+final class AssignmentTwoTest {
+    @Test
+    void pluckReturnEmptyListForNull(){
+        //setup
+        List<String> actual = AssignmentTwo.pluck(null, null);
+        //Assert
+        MatcherAssert.assertThat(actual.isEmpty(), is(true));
+
+    }
+
+
+    @Test
+    void pluckReturnsOneValueForOneInput(){
+
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        map.put("foo" , "bar");
+       // map.put("foo", "ski");
+        //setup
+        List<String> actual = AssignmentTwo.pluck(Arrays.asList(map), "foo");
+        //Assert
+        MatcherAssert.assertThat(actual, contains("bar"));
+
+    }
+
+}
